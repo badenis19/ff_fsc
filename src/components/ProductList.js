e217cd30004a3c03e84e54f35b69681e99910b8c
Bruno ADENIS <adenis.bruno@hotmail.com>
2020-07-23 13:30:34 +0100

    new fetch working with useEffect

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,62 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const ProductList = () => {
-
-  const url = 'https://www.farfetch.com/uk/plpslice/listing-api/query?setId=9645&view=180&gender=Men';
 
   let [loading, setLoading] = useState(true);
   let [products, setProductList] = useState(null);
 
-  fetch(url)
-    .then(response => response)
-    .then(response => response.json())
-    .then(data => {
-      if (data.listing.products < 0) {
-        return (
-          <p></p>
-        )
-      }
-      else {
-        let products = data.listing.products;
-        setProductList(products)
-        setLoading(false)
+  const fetchData = async () => {
+    const url = 'https://www.farfetch.com/uk/plpslice/listing-api/query?setId=9645&view=180&gender=Men';
+    const response = await fetch(url);
+    const data = await response.json();
+    let products = data.listing.products;
+    setProductList(products)
+    setLoading(false)
+  }
 
-        return (
-          <p>d</p>
-        )
-      }
-    });
+  useEffect(() => {
+    fetchData();
+  }, [])
 
-  // useEffect(() => {
-  //   const fetchData = async() => {
-  //     const url = 'https://www.farfetch.com/uk/plpslice/listing-api/query?setId=9645&view=180&gender=Men';
-  //     const response = await fetch(url);
-  //     const data = await response.json();
-  //     let products = data.listing.products;
-  //     setProductList(products)
-  //     setLoading(false)
-  //   }
-  // },[products])
-
-  // console.log(products)
+  let [filterValue, setFilterValue] = useState("Prada")
 
   const showProducts = () => {
     if (loading || !products) {
       return (
-        <p>Loading..</p>
+        <p>Loading...</p>
       )
     }
     else {
       return (
         <div className="container">
-          {products.map(product => {
+          {products.filter(a => a.brand.name === filterValue).map(product => {
             let url = "https://www.farfetch.com" + product.url;
             return (
               <div className="product" key={product.id}>
                 <a href={url}>
-                <img onMouseOver={(e) => e.target.src = product.images.cutOut} onMouseOut={(e) => e.target.src = product.images.model} src={product.images.model} alt="" />
-                <p><strong>{product.brand.name}</strong></p>
-                <p>{product.shortDescription}</p>
+                  <img onMouseOver={(e) => e.target.src = product.images.cutOut} onMouseOut={(e) => e.target.src = product.images.model} src={product.images.model} alt="" />
+                  <p><strong>{product.brand.name}</strong></p>
+                  <p>{product.shortDescription}</p>
                 </a>
               </div>
             )
@@ -66,13 +46,28 @@ const ProductList = () => {
     }
   }
 
-
+  const updateFilter = (e) => {
+    setFilterValue(e.target.value)
+  }
 
   return (
     <div>
-      {/* {getDataFromUrl()} */}
+      <form >
+        <select onChange={(e) => updateFilter(e)}>
+          {(products && !loading)
+            &&
+            products.map(product => (
+              <option key={product.id} value={product.brand.name}>
+                {product.brand.name}
+              </option>
+            ))}
+        </select>
+
+      </form>
+
       <h1>Products below:</h1>
       {showProducts()}
+
     </div>
   )
 }
